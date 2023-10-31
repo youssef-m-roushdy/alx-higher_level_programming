3f282b500ea8cde32209f374eacf3077577541ca
youssef-m-roushdy <yossefmahmoud442004@gmail.com>
2023-10-31 19:35:16 +0200

    5. 00...99

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 for i in range(0, 100):
-    print(f"{int(i / 10)}{int(i % 10)}", end='')
+    print("{}".format(int(i / 10)) + "{}".format(int(i % 10)), end='')
     if i != 99:
         print(", ", end='')
-
