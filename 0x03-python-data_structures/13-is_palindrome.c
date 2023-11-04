af9347e4b7ccc548d92aae9f1f43403c5213230a
youssef-m-roushdy <yossefmahmoud442004@gmail.com>
2023-11-04 22:25:33 +0200

    Linked list palindrome

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,25 +1,6 @@
 #include "lists.h"
 #include <stdlib.h>
 #include <stdio.h>
-
-/**
-*add_nodeint - adds a new node at the beginning of a listint_t list
-*@head: head of listint_t
-*@n: int to add in listint_t list
-*Return: address of the new element, or NULL if it failed
-*/
-listint_t *add_nodeint(listint_t **head, const int n)
-{
-	listint_t *new;
-
-	new = malloc(sizeof(listint_t));
-	if (new == NULL)
-		return (NULL);
-	new->n = n;
-	new->next = *head;
-	*head = new;
-	return (new);
-}
 /**
 *is_palindrome - identify if a syngle linked list is palindrome
 *@head: head of listint_t
