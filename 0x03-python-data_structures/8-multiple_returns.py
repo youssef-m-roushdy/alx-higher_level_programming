a355c23b1c7558f26c208268aed81943d2476e4f
youssef-m-roushdy <yossefmahmoud442004@gmail.com>
2023-11-04 19:25:54 +0200

    More returns!

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -2,5 +2,8 @@
 def multiple_returns(sentence):
     if not sentence:
         sentence = None
-    sen_len = len(sentence)
+    if sentence:
+        sen_len = len(sentence)
+    else:
+        sen_len = 0
     return (sen_len, sentence if not sentence else sentence[:1])
