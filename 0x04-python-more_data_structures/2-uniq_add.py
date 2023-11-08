78a0246d0b5c65fa4cd580091d8d61ad81a0060d
youssef-m-roushdy <yossefmahmoud442004@gmail.com>
2023-11-08 21:43:12 +0200

    Unique addition

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
-uniq_add = __import__('2-uniq_add').uniq_add
-
-my_list = [1, 2, 3, 1, 4, 2, 5]
-result = uniq_add(my_list)
-print("Result: {:d}".format(result))
+def uniq_add(my_list=[]):
+    uniq_list = set(my_list)
+    return sum(uniq_list)
