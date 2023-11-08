b5aa29185cf9a768f6782916ca5733ca4a7b8f26
youssef-m-roushdy <yossefmahmoud442004@gmail.com>
2023-11-08 22:37:50 +0200

    Print sorted dictionary

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
-best_score = __import__('10-best_score').best_score
-
-a_dictionary = {'John': 12, 'Bob': 14, 'Mike': 14, 'Molly': 16, 'Adam': 10}
-best_key = best_score(a_dictionary)
-print("Best score: {}".format(best_key))
-
-best_key = best_score(None)
-print("Best score: {}".format(best_key))
+def best_score(a_dictionary):
+    if not a_dictionary:
+        return None
+    Max = 0
+    for key, value in a_dictionary.items():
+        if value > Max:
+            Max = value
+    for key, value in a_dictionary.items():
+        if value == Max:
+            return key
