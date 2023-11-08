8a937e21f8083f89fbdd99ce279d691abba568e8
youssef-m-roushdy <yossefmahmoud442004@gmail.com>
2023-11-08 22:47:28 +0200

    Roman to Integer

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,13 @@
+def roman_to_int(roman_string):
+    if not isinstance(roman_string, str) or roman_string is None:
+        return 0
+    roman_numerals = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    prev_value, total = 0, 0
+    for numeral in roman_string[::-1]:
+        value = roman_numerals[numeral]
+        if value < prev_value:
+            total -= value
+        else:
+            total += value
+        prev_value = value
+    return total
