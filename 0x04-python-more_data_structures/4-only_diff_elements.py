bc746869f0ed10ae8b6060236a06f7acd1894eb3
youssef-m-roushdy <yossefmahmoud442004@gmail.com>
2023-11-08 22:09:24 +0200

    Only differents

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def only_diff_elements(set_1, set_2):
-    only_diff_list = list(set_1.difference(set_2)) + list(set_2.difference(set_1))
-    return only_diff_list
+    only_diff = list(set_1.difference(set_2)) + list(set_2.difference(set_1))
+    return only_diff
