db8d31271db469b863a514ca60e43f8329fc6270
youssef-m-roushdy <yossefmahmoud442004@gmail.com>
2023-11-08 21:25:40 +0200

    Search and replace

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    my_new_list = my_list[0:]
-    my_new_list[search - 1] = replace
-    return my_new_list
+    return [replace if item == search else item for item in my_list]
