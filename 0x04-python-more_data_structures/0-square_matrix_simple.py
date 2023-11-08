fbf32e56f903d5192f202e27578406776455fe15
youssef-m-roushdy <yossefmahmoud442004@gmail.com>
2023-11-08 21:10:37 +0200

    Squared simple

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,8 @@
+def square_matrix_simple(matrix=[]):
+    square_matrix = []
+    for i in range(len(matrix)):
+        row = []
+        for j in range(len(matrix[i])):
+            row.append(matrix[i][j] * matrix[i][j])
+        square_matrix.append(row)
+    return square_matrix
