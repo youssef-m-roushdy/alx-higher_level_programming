ab3458e18913e23109220a18c87aabc63dabcfc0
youssef-m-roushdy <yossefmahmoud442004@gmail.com>
2023-11-27 15:39:20 +0200

    Integers addition

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def add_integer(a, b=98):
+    if type(a) not in [int, float]:
+        raise Exception("a must be an integer")
+    elif type(b) not in [int, float]:
+        raise Exception("b must be an integer")
+    else:
+        a = int(a)
+        b = int(b)
+        return a + b
