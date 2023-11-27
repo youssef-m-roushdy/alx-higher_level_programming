69a5c13c11908b4f15bed7b04add807aef4c8d5d
youssef-m-roushdy <yossefmahmoud442004@gmail.com>
2023-11-27 17:50:07 +0200

    "Rectangle"

---
+++ 0x08-python-more_classes/5-rectangle.py
@@ -0,0 +1,139 @@
+#!/usr/bin/python3
+"""Represent class Rectangle"""
+
+
+class Rectangle:
+    """A class representing a rectangle."""
+    def __init__(self, width=0, height=0):
+        """Initializes a new Rectangle
+        instance with the given width and height.
+
+        Args:
+            width (int, optional): The width of the rectangle. Defaults to 0.
+            height (int, optional): The height of the rectangle. Defaults to 0.
+
+        Raises:
+            ValueError: If width or height is less than 0.
+            TypeError: If width or height is not an integer.
+        """
+        if type(width) == int:
+            if width >= 0:
+                self.__width = width
+            else:
+                raise ValueError("width must be >= 0")
+        else:
+            raise TypeError("width must be an integer")
+
+        if type(height) == int:
+            if height >= 0:
+                self.__height = height
+            else:
+                raise ValueError("height must be >= 0")
+        else:
+            raise TypeError("height must be an integer")
+
+    @property
+    def width(self):
+        """Gets the width of the rectangle.
+
+        Returns:
+            int: The width of the rectangle.
+        """
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Sets the width of the rectangle.
+
+        Args:
+            value (int): The new width of the rectangle.
+
+        Raises:
+            ValueError: If value is less than 0.
+            TypeError: If value is not an integer.
+        """
+        if type(value) == int:
+            if value >= 0:
+                self.__width = value
+            else:
+                raise ValueError("width must be >= 0")
+        else:
+            raise TypeError("width must be an integer")
+
+    @property
+    def height(self):
+        """Gets the height of the rectangle.
+
+        Returns:
+            int: The height of the rectangle.
+        """
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Sets the height of the rectangle.
+
+        Args:
+            value (int): The new height of the rectangle.
+
+        Raises:
+            ValueError: If value is less than 0.
+            TypeError: If value is not an integer.
+        """
+        if type(value) == int:
+            if value >= 0:
+                self.__height = value
+            else:
+                raise ValueError("height must be >= 0")
+        else:
+            raise TypeError("height must be an integer")
+
+    def area(self):
+        """Calculates and returns the area of the rectangle.
+
+        Returns:
+            int: The area of the rectangle.
+        """
+        return self.__height * self.__width
+
+    def perimeter(self):
+        """Calculates and returns the perimeter of the rectangle.
+
+        Returns:
+            int: The perimeter of the rectangle.
+        """
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        return 2 * (self.__width + self.__height)
+
+    def __str__(self):
+        """Returns a string representation of the rectangle
+            using '#' characters.
+        Returns:
+            str: The string representation of the rectangle.
+        """
+        if self.__width == 0 or self.__height == 0:
+            return ""
+        else:
+            rectangle_str = ""
+            for _ in range(self.__height):
+                rectangle_str += "#" * self.__width + "\n"
+            return rectangle_str.rstrip()
+
+    def __repr__(self):
+        """Returns a string representation of the
+            rectangle for debugging purposes.
+
+            Returns:
+                str: The string representation of the rectangle.
+        """
+        return f"Rectangle({self.__width}, {self.__height})"
+
+    def __del__(self):
+        """Returns a string representation of the rectangle
+            for debugging purposes.
+
+            Returns:
+                str: The string representation of the rectangle.
+        """
+        print("Bye rectangle...")
