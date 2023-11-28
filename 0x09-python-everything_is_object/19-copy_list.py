8b9fe44806e9e7916258c0d734d991d0d5a390ff
youssef-m-roushdy <yossefmahmoud442004@gmail.com>
2023-11-28 16:26:38 +0200

    .

--- 0x09-python-everything_is_object/19-copy_list.py
+++ 0x09-python-everything_is_object/19-copy_list.py
@@ -1,20 +1,4 @@
 #!/usr/bin/python3
 
-def copy_list(list):
-    """
-    Creates a shallow copy of the input list.
-
-    Parameters:
-    - list (list): The input list to be copied.
-
-    Returns:
-    list: A new list that is a shallow copy of the input list.
-
-    Example:
-    >>> original_list = [1, 2, 3]
-    >>> copied_list = copy_list(original_list)
-    >>> print(copied_list)
-    [1, 2, 3]
-    """
-    new_list = list[:]
-    return new_list
+def copy_list(l):
+    return l[:]
