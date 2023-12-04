94aa8daa4317589daa6c929f50d53333782d36f1
youssef-m-roushdy <yossefmahmoud442004@gmail.com>
2023-12-04 23:03:34 +0200

    My list

--- 0x0A-python-inheritance/1-main.py
+++ 0x0A-python-inheritance/1-main.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
 MyList = __import__('1-my_list').MyList
 
-# my_list = MyList()
+my_list = MyList()
 # my_list.append(1)
 # my_list.append(4)
 # my_list.append(2)
 # my_list.append(3)
 # my_list.append(5)
-my_list = MyList("hg")
 print(my_list)
 my_list.print_sorted()
 print(my_list)
