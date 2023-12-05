ade076e08c838d9da42c45978c39324fcb4639cb
youssef-m-roushdy <yossefmahmoud442004@gmail.com>
2023-12-05 11:40:47 +0200

    Same class or inherit from

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Method copare is same class or object."""
+
+
+def is_kind_of_class(obj, a_class):
+    """Method copare is same class or object."""
+    return isinstance(obj, a_class)
