47223cead236f88a30fbbdf81aca7f5ea59c3c1e
youssef-m-roushdy <yossefmahmoud442004@gmail.com>
2023-12-05 14:16:48 +0200

    Rectangle

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+"""A base class for geometric shapes."""
+
+
+class BaseGeometry:
+    """A base class for geometric shapes."""
+    def area(self):
+        """
+        Calculate the area of the geometric shape.
+
+        Raises:
+            Exception: This method is not implemented in the base class.
+        """
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """
+        Validate that the given value is a positive integer.
+
+        Args:
+            name (str): The name of the value being validated.
+            value (int): The value to be validated.
+
+        Raises:
+            TypeError: If the value is not an integer.
+            ValueError: If the value is not a positive integer.
+        """
+        if type(value) != int:
+            raise TypeError(f"{name} must be an integer")
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
+
+
+"""A class representing a rectangle."""
+
+
+class Rectangle(BaseGeometry):
+    """A class representing a rectangle."""
+    def __init__(self, width, height):
+        """
+        Initializes a new Rectangle instance.
+
+        Args:
+            width (int): The width of the rectangle.
+            height (int): The height of the rectangle.
+
+        Raises:
+            ValueError: If either width or height is not a positive integer.
+        """
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
