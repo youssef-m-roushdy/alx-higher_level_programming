0eb469605c919cb1bd82f2a7bdcbeab6e97b1894
youssef-m-roushdy <yossefmahmoud442004@gmail.com>
2023-12-06 16:07:34 +0200

    Read file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -4,9 +4,7 @@
 
 def read_file(filename=""):
     """Reads and prints the content of a file line by line."""
-    try:
-        with open(filename, "r", encoding="utf-8") as file:
-            content = file.read()
-            print(content)
-    except Exception as e:
-        print(f"Error reading the file: {e}")
+    with open(filename, mode="r", encoding="utf-8") as myFile:
+        for line in myFile:
+            print(line, end="")
+    myFile.closed
