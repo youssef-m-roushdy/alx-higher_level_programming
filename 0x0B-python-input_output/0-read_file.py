7d552aa92b3af5ade50a5880d520da8f4eb15251
youssef-m-roushdy <yossefmahmoud442004@gmail.com>
2023-12-06 16:00:34 +0200

    Read file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,29 +1,10 @@
 #!/usr/bin/python3
+"""Reads and prints the content of a file line by line."""
+
+
 def read_file(filename=""):
-    """
-    Reads and prints the content of a file line by line.
-
-    Args:
-        filename (str): The name of the file to be read. Defaults to an empty string.
-        
-    Returns:
-        None
-
-    Raises:
-        FileNotFoundError: If the specified file is not found.
-        IOError: If an error occurs while reading the file.
-
-    Example:
-        >>> read_file("example.txt")
-        This is line 1
-        This is line 2
-        This is line 3
-    """
-    try:
-        with open(filename, mode="r", encoding="utf-8") as myFile:
-            for line in myFile:
-                print(line, end="")
-    except FileNotFoundError:
-        raise FileNotFoundError(f"The file '{filename}' was not found.")
-    except IOError as e:
-        raise IOError(f"Error reading the file '{filename}': {str(e)}")
+    """Reads and prints the content of a file line by line."""
+    with open("my_file_0.txt", mode="r", encoding="utf-8") as myFile:
+        for line in myFile:
+            print(line, end="")
+        print("")
