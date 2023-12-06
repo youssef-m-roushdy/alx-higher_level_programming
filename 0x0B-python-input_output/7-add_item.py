b8810ec79b692f0cd7158b95d5cac4b748957355
youssef-m-roushdy <yossefmahmoud442004@gmail.com>
2023-12-06 19:43:34 +0200

    Load, add, save

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,20 +1,28 @@
 #!/usr/bin/python3
-"""main function."""
+"""Main script to add arguments to a Python list and save to a JSON file."""
 
 
 import sys
+from os import path
 import json
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 
-def main():
-    """main function."""
-    argv = sys.argv[1:]
-    save_to_json_file(argv, "add_item.json")
-    loaded_data = load_from_json_file("add_item.json")
-    print(loaded_data)
-
-
 if __name__ == "__main__":
-    main()
+    """Main function."""
+    # Get command line arguments (excluding the script name)
+    argv = sys.argv[1:]
+
+    # Initialize an empty list if the file doesn't exist
+    if not path.exists("add_item.json"):
+        save_to_json_file([], "add_item.json")
+
+    # Load existing data from the file
+    loaded_data = load_from_json_file("add_item.json")
+
+    # Add new items to the list
+    loaded_data.extend(argv)
+
+    # Save the updated list to the file
+    save_to_json_file(loaded_data, "add_item.json")
