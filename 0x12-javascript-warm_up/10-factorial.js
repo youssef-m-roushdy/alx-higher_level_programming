47308f199e21f851c860d573f192b089ba2fbea7
youssef-m-roushdy <yossefmahmoud442004@gmail.com>
2024-02-12 23:33:48 +0200

    Factorial

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -4,14 +4,14 @@ const { argv } = require('process');
 
 if (isNaN(Number.parseInt(argv[2], 10))) {
   console.log(1);
-}
-
-function fact (num) {
-  if (num === 1 || num === 0) {
-    return 1;
+} else {
+  function fact (num) {
+    if (num === 1 || num === 0) {
+      return 1;
+    }
+    const factor = num * fact(num - 1);
+    return factor;
   }
-  const factor = num * fact(num - 1);
-  return factor;
-}
 
-console.log(fact(Number.parseInt(argv[2], 10)));
+  console.log(fact(Number.parseInt(argv[2], 10)));
+}
