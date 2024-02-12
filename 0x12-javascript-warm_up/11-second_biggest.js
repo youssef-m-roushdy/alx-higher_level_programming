091b6bae2569009fddeebcd28dd7f0755c53def9
youssef-m-roushdy <yossefmahmoud442004@gmail.com>
2024-02-13 01:32:18 +0200

    Second biggest!

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -5,8 +5,12 @@ const args = process.argv.slice(2).map(Number);
 if (args.length <= 1) {
   console.log(0);
 } else {
-  const arr = [...args];
-  arr.sort();
-  arr.reverse();
-  console.log(arr[1]);
+  const max = Math.max(...args);
+  let secMax = 0;
+  args.forEach(num => {
+    if (max > num && num > secMax) {
+      secMax = num;
+    }
+  });
+  console.log(secMax);
 }
