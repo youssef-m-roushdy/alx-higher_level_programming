ac4d144f21a2a69eed91606d9433b37efbf3af3b
youssef-m-roushdy <yossefmahmoud442004@gmail.com>
2024-02-12 19:53:44 +0200

    Arguments

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -4,6 +4,8 @@ const { argv } = require('process');
 
 if (argv.length < 3) {
   console.log('No argument');
-} else {
+} else if (argv.length === 3) {
   console.log('Argument found');
+} else {
+  console.log('Arguments found');
 }
