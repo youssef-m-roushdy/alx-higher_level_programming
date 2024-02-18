8ed76ee0af0686617d66a5bbd840adca78f7d13a
youssef-m-roushdy <yossefmahmoud442004@gmail.com>
2024-02-18 14:07:43 +0200

    SQL Injection...

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
                          passwd=sys.argv[2], db=sys.argv[3], port=3306)
     cur = db.cursor()
     cur.execute("SELECT * FROM states WHERE name=%s ORDER BY id ASC",
-                   (sys.argv[4],))
+                (sys.argv[4],))
     rows = cur.fetchall()
     for row in rows:
         print(row)
