032d5840556f2ccfdc45e272259d2acd7af07e97
youssef-m-roushdy <yossefmahmoud442004@gmail.com>
2024-02-18 19:29:36 +0200

    Contains `a`

---
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+Start link class to table in database
+"""
+
+import sys
+from model_state import Base, State
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    states_with_a = session.query(State)\
+        .filter(State.name.like('%a%'))\
+        .order_by(State.id).all()
+    if states_with_a:
+        for state in states_with_a:
+            print("{}: {}".format(state.id, state.name))
+    else:
+        print("Nothing")
+    session.close()
