d805f8334d68fc799ec7f81274a6de89f2538996
youssef-m-roushdy <yossefmahmoud442004@gmail.com>
2024-03-03 23:18:01 +0200

    .

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-# takes in a URL, sends a POST request to the passed URL, and displays the body of the response
-email="test@gmail.com"
-subject="I will always be here for PLD"
-curl -X POST -d "email=$email&subject=$subject" "$1"
+# script to post data (url-encoded) to a server
+curl -s -d "email=hr@holbertonschool.com&subject=I%20will%20always%20be%20here%20for%20PLD" -X POST "$1"
