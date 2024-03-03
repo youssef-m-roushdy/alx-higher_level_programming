5483f151191d5af80c02bd0230ec4d41152e9c66
youssef-m-roushdy <yossefmahmoud442004@gmail.com>
2024-03-03 21:41:15 +0200

    .

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# takes in a URL, sends a request to that URL, and displays the size of the body of the response
-curl -sX GET $1 -L
+# shellcheck disable=SC2046
+if [ $(curl -L -s -X HEAD -w "%{http_code}" "$1") == '200' ]; then curl -Ls "$1"; fi
