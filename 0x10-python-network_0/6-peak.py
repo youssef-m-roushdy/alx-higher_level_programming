a5c66150ba8b857c92cade02129b90d04c09a497
youssef-m-roushdy <yossefmahmoud442004@gmail.com>
2024-03-03 23:10:06 +0200

    .

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -14,14 +14,11 @@ def find_peak(list_of_integers):
     Returns:
     int: The peak integer.
     """
-    # Check if the list is empty
     if not list_of_integers:
         return None
 
-    # Initialize the peak variable
     peak = list_of_integers[0]
 
-    # Iterate through the list to find the peak
     for i in range(1, len(list_of_integers)):
         if list_of_integers[i] > peak:
             peak = list_of_integers[i]
