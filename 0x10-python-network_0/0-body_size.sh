944838a62ace997a9d288f0a9b22b641d61dc8a9
youssef-m-roushdy <yossefmahmoud442004@gmail.com>
2024-03-03 19:47:48 +0200

    .

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,16 +1,4 @@
 #!/bin/bash
-
-# Check if the user provided a URL
-if [ -z "$1" ]; then
-  echo "Usage: $0 <URL>"
-  exit 1
-fi
-
-# Send a request to the URL using curl and store the response body in a variable
-response=$(curl -s "$1")
-
-# Calculate the size of the response body in bytes
-size=$(echo -n "$response" | wc -c)
-
-# Display the size of the response body in bytes
-echo "$size"
+URL=$1
+res=$(curl -sI $URL | grep -i content-length)
+echo "$res"
