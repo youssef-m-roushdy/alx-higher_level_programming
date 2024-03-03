8b63b10d4bf0dda593bdd6ab76b577fe05d23301
youssef-m-roushdy <yossefmahmoud442004@gmail.com>
2024-03-03 23:25:49 +0200

    .

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Script that takes in a URL, sends a GET request to the URL, and displays the body of the response
-curl -sL "$1"
+# takes in a URL, sends a request to that URL, and displays the size of the body of the response
+curl -sX GET $1 -L
