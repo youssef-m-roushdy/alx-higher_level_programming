e56e9e21f2e89b7cdf7d52d86f58e5ab90ba39ba
youssef-m-roushdy <yossefmahmoud442004@gmail.com>
2024-03-04 18:36:31 +0200

    .

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -10,6 +10,5 @@ import sys
 if __name__ == "__main__":
     url = sys.argv[1]
 
-    request = urllib.request.Request(url)
     with urllib.request.urlopen(url) as response:
         print(response.headers['X-Request-Id'])
