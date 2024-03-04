bc54538072cb0fe7bc8e82c1c6027c18612e7aca
youssef-m-roushdy <yossefmahmoud442004@gmail.com>
2024-03-04 19:50:08 +0200

    .

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 import requests
 import sys
-
+"""
+Python script that takes your GitHub credentials (username and password) and
+uses the GitHub API to display your id
+"""
 
 if __name__ == '__main__':
     if len(sys.argv) != 3:
@@ -10,9 +13,7 @@ if __name__ == '__main__':
 
     username = sys.argv[1]
     personal_access_token = sys.argv[2]
-
     url = f'https://api.github.com/users/{username}'
     headers = {'Authorization': f'token {personal_access_token}'}
     response = requests.get(url, headers=headers)
-
     print(response.json().get('id'))
