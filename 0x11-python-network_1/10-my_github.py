a8980ba1efabfc6f0a25276ebb6d503076138ed8
youssef-m-roushdy <yossefmahmoud442004@gmail.com>
2024-03-04 19:49:09 +0200

    .

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -2,14 +2,6 @@
 import requests
 import sys
 
-def get_github_id(username, personal_access_token):
-    url = f'https://api.github.com/users/{username}'
-    headers = {'Authorization': f'token {personal_access_token}'}
-    response = requests.get(url, headers=headers)
-    if response.status_code == 200:
-        return response.json().get('id')
-    else:
-        return None
 
 if __name__ == '__main__':
     if len(sys.argv) != 3:
@@ -19,5 +11,8 @@ if __name__ == '__main__':
     username = sys.argv[1]
     personal_access_token = sys.argv[2]
 
-    github_id = get_github_id(username, personal_access_token)
-    print(github_id)
+    url = f'https://api.github.com/users/{username}'
+    headers = {'Authorization': f'token {personal_access_token}'}
+    response = requests.get(url, headers=headers)
+
+    print(response.json().get('id'))
