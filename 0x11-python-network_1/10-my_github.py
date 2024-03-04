fc23f598479789bceeb5fe5f765bc9af243a4d29
youssef-m-roushdy <yossefmahmoud442004@gmail.com>
2024-03-04 19:52:25 +0200

    .

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -7,10 +7,7 @@ uses the GitHub API to display your id
 """
 
 if __name__ == '__main__':
-    if len(sys.argv) != 3:
-        print('Usage: ./10-my_github.py <username> <personal_access_token>')
-        sys.exit(1)
-
+    
     username = sys.argv[1]
     personal_access_token = sys.argv[2]
     url = f'https://api.github.com/users/{username}'
