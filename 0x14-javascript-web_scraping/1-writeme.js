3fc806c13d7830aa1e495d7f68428ce2afb7cc1b
youssef-m-roushdy <yossefmahmoud442004@gmail.com>
2024-03-27 21:35:05 +0200

    .

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -2,13 +2,12 @@
 
 const fs = require('fs');
 
-
 const filePath = process.argv[2];
 
-const msg = process.argv[3];;
+const msg = process.argv[3];
 
 fs.writeFile(filePath, msg, (err) => {
-  if(err) {
+  if (err) {
     console.error(err);
   }
 });
