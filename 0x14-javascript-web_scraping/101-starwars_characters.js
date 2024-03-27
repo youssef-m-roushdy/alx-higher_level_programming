c8723293e0ffc47bc4f0aa353070041377e585a0
youssef-m-roushdy <yossefmahmoud442004@gmail.com>
2024-03-28 01:18:32 +0200

    .

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -7,15 +7,25 @@ const url = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
 request(url, function (error, response, body) {
   if (error) {
     console.error(error);
+    return;
   }
   const data = JSON.parse(body);
-  data.characters.forEach((character) => {
-    request(character, (error, response, body) => {
-      if (error) {
-        console.error(error);
-      }
-      const data = JSON.parse(body);
-      console.log(data.name);
-    });
-  });
+  const characters = data.characters;
+
+  const getData = async () => {
+    for (const characterUrl of characters) {
+      await new Promise((resolve, reject) => {
+        request(characterUrl, (error, response, body) => {
+          if (error) {
+            console.error(error);
+            reject(error);
+          }
+          const data = JSON.parse(body);
+          console.log(data.name);
+          resolve();
+        });
+      });
+    }
+  };
+  getData();
 });
