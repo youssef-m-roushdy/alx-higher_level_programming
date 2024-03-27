e1a573478f72df7b205b0218d423e6fd500167d1
youssef-m-roushdy <yossefmahmoud442004@gmail.com>
2024-03-27 22:09:20 +0200

    .

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+const url = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
+
+request(url, function (error, response, body) {
+  if (error) {
+    console.error(error);
+  }
+  const data = JSON.parse(body);
+  console.log(data.title);
+});
