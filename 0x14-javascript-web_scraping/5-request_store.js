88b7d40ab44e29540e227780568609fe06c26fe9
youssef-m-roushdy <yossefmahmoud442004@gmail.com>
2024-03-28 00:15:47 +0200

    .

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+
+const request = require('request');
+const fs = require('fs');
+
+const url = process.argv[2];
+const filePath = process.argv[3];
+
+request(url, function (error, response, body) {
+  if (error) {
+    console.error(error);
+  }
+  fs.writeFile(filePath, body, function (err) {
+    if (err) {
+      console.error(err);
+      process.exit(1);
+    }
+  });
+});
