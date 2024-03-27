a487270f91e37a4a3f8a0abb62f1c2231d85fa82
youssef-m-roushdy <yossefmahmoud442004@gmail.com>
2024-03-27 21:29:43 +0200

    .

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+const fs = require('fs');
+
+const filePath = process.argv[2];
+
+fs.readFile(filePath, 'utf8', (err, data) => {
+  if (err) {
+    console.error('Error reading file:', err);
+    return;
+  }
+  console.log(data);
+});
