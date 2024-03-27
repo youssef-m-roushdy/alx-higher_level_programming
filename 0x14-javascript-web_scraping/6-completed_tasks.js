bb112a3b8dcae99c2918750aeec02159be009eb3
youssef-m-roushdy <yossefmahmoud442004@gmail.com>
2024-03-28 00:37:21 +0200

    .

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -2,23 +2,24 @@
 
 const request = require('request');
 
-const url = process.argv[2];
+request(process.argv[2], function (err, _res, body) {
+  if (err) {
+    console.log(err);
+  } else {
+    const completedTasksByUsers = {};
+    body = JSON.parse(body);
 
-request(url, function (error, response, body) {
-  if (error) {
-    console.error(error);
+    for (let i = 0; i < body.length; ++i) {
+      const userId = body[i].userId;
+      const completed = body[i].completed;
+
+      if (completed && !completedTasksByUsers[userId]) {
+        completedTasksByUsers[userId] = 0;
+      }
+
+      if (completed) ++completedTasksByUsers[userId];
+    }
+
+    console.log(completedTasksByUsers);
   }
-  const completedTasksByUsers = {};
-  const data = JSON.parse(body);
-  console.log(data);
-  data.forEach((user) => {
-    if (user.completed && !completedTasksByUsers[user.userId]) {
-      completedTasksByUsers[user.userId] = 0;
-    }
-
-    if (user.completed) {
-      ++completedTasksByUsers[user.userId];
-    }
-  });
-  console.log(completedTasksByUsers);
 });
